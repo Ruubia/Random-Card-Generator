ba0407bfba19f259d429059f8d495bac78a757d1
Beatriz Carmona Jurado <b.carmonaj@gmail.com>
2024-07-25 18:57:33 +0000

    he modificado los botones

--- src/app.js
+++ src/app.js
@@ -13,18 +13,32 @@ window.onload = function() {
 };
 
 function generateRandomCard() {
-  const suits = ['heart', 'spade', 'club', 'diamond'];
-  const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+  const suits = ["heart", "spade", "club", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
   const symbols = {
-    'heart': '♥',
-    'spade': '♠',
-    'club': '♣',
-    'diamond': '♦'
+    heart: "♥",
+    spade: "♠",
+    club: "♣",
+    diamond: "♦"
   };
   const randomSuit = suits[Math.floor(Math.random() * suits.length)];
   const randomValue = values[Math.floor(Math.random() * values.length)];
 
-  const cardElement = document.getElementById('card');
+  const cardElement = document.getElementById("card");
   cardElement.className = `card ${randomSuit}`;
   cardElement.innerHTML = `
     <div class="top-left">${randomValue}<br>${symbols[randomSuit]}</div>
@@ -34,10 +48,10 @@ function generateRandomCard() {
 }
 
 function setCardSize() {
-  const widthInput = document.getElementById('width-input').value;
-  const heightInput = document.getElementById('height-input').value;
+  const widthInput = document.getElementById("width-input").value;
+  const heightInput = document.getElementById("height-input").value;
 
-  const cardElement = document.getElementById('card');
+  const cardElement = document.getElementById("card");
   if (widthInput) {
     cardElement.style.width = `${widthInput}px`;
   }
@@ -45,3 +59,8 @@ function setCardSize() {
     cardElement.style.height = `${heightInput}px`;
   }
 }
+
+document
+  .getElementById("generateNewCard")
+  .addEventListener("click", generateRandomCard);
+document.getElementById("setSize").addEventListener("click", setCardSize);
